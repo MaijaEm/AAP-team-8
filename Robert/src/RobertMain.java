8382ad34600a29d23c59c753f668dca669d3b89e
kiia22004 <113587481+kiia22004@users.noreply.github.com>
2023-04-17 12:53:43 +0300

    Update RobertMain.java
    Added comments

---
+++ Robert/src/RobertMain.java
@@ -0,0 +1,23 @@
+
+
+public class RobertMain {
+	
+	//Creating local variables
+
+	private static DataExchange DE;
+	private static ColorSensor CSObj;
+	private static UltraSonic USObj;
+	private static Motor MObj;
+	
+	//Creating the main method
+	
+	public static void main(String[] args) {
+		DE = new DataExchange();
+		USObj = new UltraSonic(DE);
+		CSObj = new ColorSensor(DE);
+		MObj = new Motor(DE);
+		USObj.start();
+		CSObj.start();
+		MObj.start();
+	}
+}
